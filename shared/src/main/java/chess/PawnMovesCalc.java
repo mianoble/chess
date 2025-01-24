2247083b0d5e46abaa1a4ff20244ae3605fb5030
Mialani Noble <miamno20@gmail.com>
2025-01-24 11:12:12 -0700

    pawn done

--- shared/src/main/java/chess/PawnMovesCalc.java
+++ shared/src/main/java/chess/PawnMovesCalc.java
@@ -22,38 +22,93 @@ public class PawnMovesCalc extends PieceMovesCalculator{
                     row ++;
                     // check for obstacles
                     ChessPosition checkPos = new ChessPosition(row, col);
-                    if (board.getPiece(checkPos) != null) { //if there is something in the position
-                        if (myPiece.getTeamColor() != board.getPiece(checkPos).getTeamColor()) {
-                            ChessMove move = new ChessMove(myPosition, checkPos);
-                            possibleMoves.add(move);
-                            break;
-                        } else
-                            break;
+                    if (board.getPiece(checkPos) == null) { //if there is something in the position
+                        ChessPosition newPosition = new ChessPosition(row, col);
+                        ChessMove move = new ChessMove(myPosition, newPosition);
+                        possibleMoves.add(move);
                     }
-                    ChessPosition newPosition = new ChessPosition(row, col);
-                    ChessMove move = new ChessMove(myPosition, newPosition);
-                    possibleMoves.add(move);
+                    else {
+                        break;
+                    }
+
                 }
                 // check two diagonals
                 row = myPosition.getRow();
                 row++; col--; // diagonal to the left
                 ChessPosition checkPos = new ChessPosition(row, col);
-                if (board.getPiece(checkPos) != null) {
-                    if (myPiece.getTeamColor() != board.getPiece(checkPos).getTeamColor()) {
-                        ChessMove move = new ChessMove(myPosition, checkPos);
-                        possibleMoves.add(move);
+                // check within board bounds
+                if (row > 0 && row <= 8 && col > 0 && col <= 8) {
+                    if (board.getPiece(checkPos) != null) {
+                        if (myPiece.getTeamColor() != board.getPiece(checkPos).getTeamColor()) {
+                            ChessMove move = new ChessMove(myPosition, checkPos);
+                            possibleMoves.add(move);
+                        }
+                    }
+
+                    col += 2; // diagonal to the right
+                    ChessPosition checkPos2 = new ChessPosition(row, col);
+                    if (board.getPiece(checkPos2) != null) {
+                        if (myPiece.getTeamColor() != board.getPiece(checkPos2).getTeamColor()) {
+                            ChessMove move = new ChessMove(myPosition, checkPos2);
+                            possibleMoves.add(move);
+                        }
                     }
                 }
 
-                col+=2; // diagonal to the right
+
+            }
+            else if (row == 7) { // close to other side of the board
+                // check if pos in front is open and add with promotion
+                row = myPosition.getRow();
+                col = myPosition.getColumn();
+                row++;
+                ChessPosition checkPos = new ChessPosition(row, col);
+                if (board.getPiece(checkPos) == null) {
+                    ChessMove moveR = new ChessMove(myPosition, checkPos, ChessPiece.PieceType.ROOK);
+                    possibleMoves.add(moveR);
+                    ChessMove moveN = new ChessMove(myPosition, checkPos, ChessPiece.PieceType.KNIGHT);
+                    possibleMoves.add(moveN);
+                    ChessMove moveB = new ChessMove(myPosition, checkPos, ChessPiece.PieceType.BISHOP);
+                    possibleMoves.add(moveB);
+                    ChessMove moveQ = new ChessMove(myPosition, checkPos, ChessPiece.PieceType.QUEEN);
+                    possibleMoves.add(moveQ);
+                }
+                // check if two diagonals is occupied and add with promotion
+                col--;
                 ChessPosition checkPos2 = new ChessPosition(row, col);
-                if (board.getPiece(checkPos2) != null) {
-                    if (myPiece.getTeamColor() != board.getPiece(checkPos2).getTeamColor()) {
-                        ChessMove move = new ChessMove(myPosition, checkPos2);
-                        possibleMoves.add(move);
+                // check within board bounds
+                if (row > 0 && row <= 8 && col > 0 && col <= 8) {
+                    if (board.getPiece(checkPos2) != null) {
+                        if (myPiece.getTeamColor() != board.getPiece(checkPos2).getTeamColor()) {
+                            ChessMove moveR = new ChessMove(myPosition, checkPos2, ChessPiece.PieceType.ROOK);
+                            possibleMoves.add(moveR);
+                            ChessMove moveN = new ChessMove(myPosition, checkPos2, ChessPiece.PieceType.KNIGHT);
+                            possibleMoves.add(moveN);
+                            ChessMove moveB = new ChessMove(myPosition, checkPos2, ChessPiece.PieceType.BISHOP);
+                            possibleMoves.add(moveB);
+                            ChessMove moveQ = new ChessMove(myPosition, checkPos2, ChessPiece.PieceType.QUEEN);
+                            possibleMoves.add(moveQ);
+                        }
                     }
                 }
 
+                col += 2;
+                ChessPosition checkPos3 = new ChessPosition(row, col);
+                // check within board bounds
+                if (row > 0 && row <= 8 && col > 0 && col <= 8) {
+                    if (board.getPiece(checkPos3) != null) {
+                        if (myPiece.getTeamColor() != board.getPiece(checkPos3).getTeamColor()) {
+                            ChessMove moveR = new ChessMove(myPosition, checkPos3, ChessPiece.PieceType.ROOK);
+                            possibleMoves.add(moveR);
+                            ChessMove moveN = new ChessMove(myPosition, checkPos3, ChessPiece.PieceType.KNIGHT);
+                            possibleMoves.add(moveN);
+                            ChessMove moveB = new ChessMove(myPosition, checkPos3, ChessPiece.PieceType.BISHOP);
+                            possibleMoves.add(moveB);
+                            ChessMove moveQ = new ChessMove(myPosition, checkPos3, ChessPiece.PieceType.QUEEN);
+                            possibleMoves.add(moveQ);
+                        }
+                    }
+            }
 
             }
             else { // if its not in the starting position
@@ -70,19 +125,26 @@ public class PawnMovesCalc extends PieceMovesCalculator{
                 // check if two diagonals is occupied and add
                 col--;
                 ChessPosition checkPos2 = new ChessPosition(row, col);
-                if (board.getPiece(checkPos2) != null) {
-                    if (myPiece.getTeamColor() != board.getPiece(checkPos2).getTeamColor()) {
-                        ChessMove move = new ChessMove(myPosition, checkPos2);
-                        possibleMoves.add(move);
+                // check within board bounds
+                if (row > 0 && row <= 8 && col > 0 && col <= 8) {
+                    if (board.getPiece(checkPos2) != null) {
+                        if (myPiece.getTeamColor() != board.getPiece(checkPos2).getTeamColor()) {
+                            ChessMove move = new ChessMove(myPosition, checkPos2);
+                            possibleMoves.add(move);
+                        }
                     }
                 }
 
+
                 col+=2;
                 ChessPosition checkPos3 = new ChessPosition(row, col);
-                if (board.getPiece(checkPos3) != null) {
-                    if (myPiece.getTeamColor() != board.getPiece(checkPos3).getTeamColor()) {
-                        ChessMove move = new ChessMove(myPosition, checkPos3);
-                        possibleMoves.add(move);
+                // check within board bounds
+                if (row > 0 && row <= 8 && col > 0 && col <= 8) {
+                    if (board.getPiece(checkPos3) != null) {
+                        if (myPiece.getTeamColor() != board.getPiece(checkPos3).getTeamColor()) {
+                            ChessMove move = new ChessMove(myPosition, checkPos3);
+                            possibleMoves.add(move);
+                        }
                     }
                 }
             }
@@ -96,37 +158,95 @@ public class PawnMovesCalc extends PieceMovesCalculator{
                     row --;
                     // check for obstacles
                     ChessPosition checkPos = new ChessPosition(row, col);
-                    if (board.getPiece(checkPos) != null) { //if there is something in the position
-                        if (myPiece.getTeamColor() != board.getPiece(checkPos).getTeamColor()) {
-                            ChessMove move = new ChessMove(myPosition, checkPos);
-                            possibleMoves.add(move);
-                            break;
-                        } else
-                            break;
+                    if (board.getPiece(checkPos) == null) { //if there is something in the position
+                        ChessPosition newPosition = new ChessPosition(row, col);
+                        ChessMove move = new ChessMove(myPosition, newPosition);
+                        possibleMoves.add(move);
                     }
-                    ChessPosition newPosition = new ChessPosition(row, col);
-                    ChessMove move = new ChessMove(myPosition, newPosition);
-                    possibleMoves.add(move);
+                    else {
+                        break;
+                    }
+
                 }
                 // check two diagonals
                 row = myPosition.getRow();
                 row--; col--; // down to the left
                 ChessPosition checkPos = new ChessPosition(row, col);
-                if (board.getPiece(checkPos) != null) {
-                    if (myPiece.getTeamColor() != board.getPiece(checkPos).getTeamColor()) {
-                        ChessMove move = new ChessMove(myPosition, checkPos);
-                        possibleMoves.add(move);
+                // check within board bounds
+                if (row > 0 && row <= 8 && col > 0 && col <= 8) {
+                    if (board.getPiece(checkPos) != null) {
+                        if (myPiece.getTeamColor() != board.getPiece(checkPos).getTeamColor()) {
+                            ChessMove move = new ChessMove(myPosition, checkPos);
+                            possibleMoves.add(move);
+                        }
                     }
                 }
 
                 col+=2; // diagonal to the right
                 ChessPosition checkPos2 = new ChessPosition(row, col);
-                if (board.getPiece(checkPos2) != null) {
-                    if (myPiece.getTeamColor() != board.getPiece(checkPos2).getTeamColor()) {
-                        ChessMove move = new ChessMove(myPosition, checkPos2);
-                        possibleMoves.add(move);
+                // check within board bounds
+                if (row > 0 && row <= 8 && col > 0 && col <= 8) {
+                    if (board.getPiece(checkPos2) != null) {
+                        if (myPiece.getTeamColor() != board.getPiece(checkPos2).getTeamColor()) {
+                            ChessMove move = new ChessMove(myPosition, checkPos2);
+                            possibleMoves.add(move);
+                        }
                     }
                 }
+            }
+            else if (row == 2){ // close to other side of the board
+                // check if pos in front is open and add with promotion
+                row = myPosition.getRow();
+                col = myPosition.getColumn();
+                row--;
+                ChessPosition checkPos = new ChessPosition(row, col);
+                if (board.getPiece(checkPos) == null) {
+                    ChessMove moveR = new ChessMove(myPosition, checkPos, ChessPiece.PieceType.ROOK);
+                    possibleMoves.add(moveR);
+                    ChessMove moveN = new ChessMove(myPosition, checkPos, ChessPiece.PieceType.KNIGHT);
+                    possibleMoves.add(moveN);
+                    ChessMove moveB = new ChessMove(myPosition, checkPos, ChessPiece.PieceType.BISHOP);
+                    possibleMoves.add(moveB);
+                    ChessMove moveQ = new ChessMove(myPosition, checkPos, ChessPiece.PieceType.QUEEN);
+                    possibleMoves.add(moveQ);
+                }
+                // check if two diagonals is occupied and add with promotion
+                col--;
+                ChessPosition checkPos2 = new ChessPosition(row, col);
+                // check within board bounds
+                if (row > 0 && row <= 8 && col > 0 && col <= 8) {
+                    if (board.getPiece(checkPos2) != null) {
+                        if (myPiece.getTeamColor() != board.getPiece(checkPos2).getTeamColor()) {
+                            ChessMove moveR = new ChessMove(myPosition, checkPos2, ChessPiece.PieceType.ROOK);
+                            possibleMoves.add(moveR);
+                            ChessMove moveN = new ChessMove(myPosition, checkPos2, ChessPiece.PieceType.KNIGHT);
+                            possibleMoves.add(moveN);
+                            ChessMove moveB = new ChessMove(myPosition, checkPos2, ChessPiece.PieceType.BISHOP);
+                            possibleMoves.add(moveB);
+                            ChessMove moveQ = new ChessMove(myPosition, checkPos2, ChessPiece.PieceType.QUEEN);
+                            possibleMoves.add(moveQ);
+                        }
+                    }
+                }
+
+                col+=2;
+                ChessPosition checkPos3 = new ChessPosition(row, col);
+                // check within board bounds
+                if (row > 0 && row <= 8 && col > 0 && col <= 8) {
+                    if (board.getPiece(checkPos3) != null) {
+                        if (myPiece.getTeamColor() != board.getPiece(checkPos3).getTeamColor()) {
+                            ChessMove moveR = new ChessMove(myPosition, checkPos3, ChessPiece.PieceType.ROOK);
+                            possibleMoves.add(moveR);
+                            ChessMove moveN = new ChessMove(myPosition, checkPos3, ChessPiece.PieceType.KNIGHT);
+                            possibleMoves.add(moveN);
+                            ChessMove moveB = new ChessMove(myPosition, checkPos3, ChessPiece.PieceType.BISHOP);
+                            possibleMoves.add(moveB);
+                            ChessMove moveQ = new ChessMove(myPosition, checkPos3, ChessPiece.PieceType.QUEEN);
+                            possibleMoves.add(moveQ);
+                        }
+                    }
+                }
+
             }
             else { // if its not in the starting position
                 // check if pos in front is open and add
@@ -142,19 +262,25 @@ public class PawnMovesCalc extends PieceMovesCalculator{
                 // check if two diagonals is occupied and add
                 col--;
                 ChessPosition checkPos2 = new ChessPosition(row, col);
-                if (board.getPiece(checkPos2) != null) {
-                    if (myPiece.getTeamColor() != board.getPiece(checkPos2).getTeamColor()) {
-                        ChessMove move = new ChessMove(myPosition, checkPos2);
-                        possibleMoves.add(move);
+                // check within board bounds
+                if (row > 0 && row <= 8 && col > 0 && col <= 8) {
+                    if (board.getPiece(checkPos2) != null) {
+                        if (myPiece.getTeamColor() != board.getPiece(checkPos2).getTeamColor()) {
+                            ChessMove move = new ChessMove(myPosition, checkPos2);
+                            possibleMoves.add(move);
+                        }
                     }
                 }
 
                 col+=2;
                 ChessPosition checkPos3 = new ChessPosition(row, col);
-                if (board.getPiece(checkPos3) != null) {
-                    if (myPiece.getTeamColor() != board.getPiece(checkPos3).getTeamColor()) {
-                        ChessMove move = new ChessMove(myPosition, checkPos3);
-                        possibleMoves.add(move);
+                // check within board bounds
+                if (row > 0 && row <= 8 && col > 0 && col <= 8) {
+                    if (board.getPiece(checkPos3) != null) {
+                        if (myPiece.getTeamColor() != board.getPiece(checkPos3).getTeamColor()) {
+                            ChessMove move = new ChessMove(myPosition, checkPos3);
+                            possibleMoves.add(move);
+                        }
                     }
                 }
 
