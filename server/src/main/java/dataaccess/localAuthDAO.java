f98d862e37417cf80c1e70bf6d9bbe7b9016fe8a
Mialani Noble <miamno20@gmail.com>
2025-03-02 10:58:36 -0700

    dao interfaces

--- server/src/main/java/dataaccess/localAuthDAO.java
+++ server/src/main/java/dataaccess/localAuthDAO.java
@@ -1,4 +1,30 @@
 package dataaccess;
 
-public class localAuthDAO {
+import Model.AuthData;
+
+import java.util.HashSet;
+
+public class localAuthDAO implements AuthTokenDAO {
+
+    HashSet<AuthData> authDataSet;
+    
+    @Override
+    public void createAuth(AuthData authData) throws DataAccessException {
+
+    }
+
+    @Override
+    public AuthData getAuth(String authToken) throws DataAccessException {
+        return null;
+    }
+
+    @Override
+    public void deleteAuth(String authToken) throws DataAccessException {
+
+    }
+
+    @Override
+    public void clear() throws DataAccessException {
+
+    }
 }
