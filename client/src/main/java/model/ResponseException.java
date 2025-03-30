865bee6587a48c04d4ff60757cbeb474b37429f6
Mialani Noble <miamno20@gmail.com>
2025-03-29 19:22:09 -0600

    fixing dependencies

--- client/src/main/java/model/ResponseException.java
+++ client/src/main/java/model/ResponseException.java
@@ -1,7 +1,5 @@
 package model;
 
-import dataaccess.DataAccessException;
-
 public class ResponseException  extends DataAccessException {
     final private int statusCode;
     public ResponseException(int statusCode, String message) {
